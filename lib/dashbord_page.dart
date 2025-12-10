762c2dcd601c697b5d5e90b2b19fd09af9723a64
abdullahsheikh6835-beep <abdullahsheikh6835@gmail.com>
2025-12-10 22:27:06 +0500

    'git hub'

--- lib/dashbord_page.dart
+++ lib/dashbord_page.dart
@@ -1,88 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:notesapp/auth_repo.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:notesapp/components%20folder/custom_field.dart';
-import 'package:notesapp/components%20folder/custom_text.dart';
-import 'package:notesapp/floder_page.dart';
-import 'package:notesapp/utils/app_colors.dart';
+import 'package:notesapp/fire_store/repo_class.dart';
 
-class Dashboardscreen extends StatefulWidget {
-  Dashboardscreen({super.key, this.userData});
-
-  Map<String, dynamic>? userData;
+class text_page extends StatefulWidget {
+  const text_page({super.key});
 
   @override
-  State<Dashboardscreen> createState() => _DashboardscreenState();
+  State<text_page> createState() => _text_pageState();
 }
 
-class _DashboardscreenState extends State<Dashboardscreen> {
-  TextEditingController searchcontroler = TextEditingController();
-
+class _text_pageState extends State<text_page> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController workcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Folderscreen()),
-          );
-        },
-      ),
       appBar: AppBar(
-        leading: TextButton(
-            onPressed: () {},
-            child: IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Folderscreen()));
-                },
-                icon: Icon(Icons.arrow_back))),
-        title: CustomText(text: 'Notes', Color: AppColors.amber, fontSize: 15),
-        centerTitle: true,
+        title: Text('text page',),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 10),
-            customTextField(
-              hint: 'search',
-              controller: searchcontroler,
-            ),
-            StreamBuilder(
-              stream: Auth().fetchNotesData(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Text("No Notes found");
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-                if (snapshot.connectionState == ConnectionState.active) {
-                  if (snapshot.data != null) {
-                    final data = snapshot.data;
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: data?.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            Text(" ${data?[index]["Notes"]}"),
-                            Text(data?[index]["Tasks"]),
-                            Text(data?[index]["Text"]),
-                            Text(data?[index]["id"]),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                }
-                return Text("");
-              },
-            ),
-          ],
-        ),
-      ),
+      body:StreamBuilder(stream: authrepo().obj, builder: (context,Snapshot){
+        if(Snapshot.hasData){
+          return Text('No notes found');
+        }
+        if(Snapshot.connectionState == ConnectionState.waiting){
+          return CircularProgressIndicator();
+        }if(Snapshot.data != null){
+          final data = Snapshot.data;
+          return ListView.builder(itemCount:data!.length, itemBuilder: (context , index){
+                  return Column(
+                    children: [
+                      Text('${data?[index]}'),
+                      Text(data?[index]['name']),
+                      Text(data?[index]['work'])
+
+                    ],
+                  );
+          });
+          }return Text('');
+      })
     );
   }
 }
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:notesapp/auth_repo.dart';
+// import 'package:notesapp/components%20folder/custom_field.dart';
+// import 'package:notesapp/components%20folder/custom_text.dart';
+// import 'package:notesapp/floder_page.dart';
+// import 'package:notesapp/utils/app_colors.dart';
+
+// class Dashboardscreen extends StatefulWidget {
+//   Dashboardscreen({super.key, this.userData});
+
+//   Map<String, dynamic>? userData;
+
+//   @override
+//   State<Dashboardscreen> createState() => _DashboardscreenState();
+// }
+
+// class _DashboardscreenState extends State<Dashboardscreen> {
+//   TextEditingController searchcontroler = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(builder: (context) => Folderscreen()),
+//           );
+//         },
+//       ),
+//       appBar: AppBar(
+//         leading: TextButton(
+//             onPressed: () {},
+//             child: IconButton(
+//                 onPressed: () {
+//                   Navigator.push(context,
+//                       MaterialPageRoute(builder: (context) => Folderscreen()));
+//                 },
+//                 icon: Icon(Icons.arrow_back))),
+//         title: CustomText(text: 'Notes', Color: AppColors.amber, fontSize: 15),
+//         centerTitle: true,
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: <Widget>[
+//             SizedBox(height: 10),
+//             customTextField(
+//               hint: 'search',
+//               controller: searchcontroler,
+//             ),
+//             StreamBuilder(
+//               stream: Auth().fetchNotesData(),
+//               builder: (context, snapshot) {
+//                 if (!snapshot.hasData) {
+//                   return Text("No Notes found");
+//                 }
+//                 if (snapshot.connectionState == ConnectionState.waiting) {
+//                   return CircularProgressIndicator();
+//                 }
+//                 if (snapshot.connectionState == ConnectionState.active) {
+//                   if (snapshot.data != null) {
+//                     final data = snapshot.data;
+//                     return ListView.builder(
+//                       shrinkWrap: true,
+//                       itemCount: data?.length,
+//                       itemBuilder: (context, index) {
+//                         return Column(
+//                           children: [
+//                             Text(" ${data?[index]["Notes"]}"),
+//                             Text(data?[index]["Tasks"]),
+//                             Text(data?[index]["Text"]),
+//                             Text(data?[index]["id"]),
+//                           ],
+//                         );
+//                       },
+//                     );
+//                   }
+//                 }
+//                 return Text("");
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/floder_page.dart
+++ lib/floder_page.dart
@@ -1,86 +1,144 @@
 import 'package:flutter/material.dart';
-import 'package:notesapp/auth_repo.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:notesapp/components%20folder/custom_field.dart';
 import 'package:notesapp/components%20folder/custom_text.dart';
 import 'package:notesapp/dashbord_page.dart';
+import 'package:notesapp/fire_store/repo_class.dart';
 import 'package:notesapp/utils/app_colors.dart';
 
-class Folderscreen extends StatefulWidget {
-  Folderscreen({super.key});
+class store_page extends StatefulWidget {
+  const store_page({super.key});
 
   @override
-  State<Folderscreen> createState() => _FolderscreenState();
+  State<store_page> createState() => _store_pageState();
 }
 
-class _FolderscreenState extends State<Folderscreen> {
-  TextEditingController textcontroler = TextEditingController();
-
-  TextEditingController notescontroler = TextEditingController();
-
-  TextEditingController taskcontroler = TextEditingController();
+class _store_pageState extends State<store_page> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController workcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomText(
-                  text: 'Folders adding', Color: AppColors.black, fontSize: 18),
-              SizedBox(height: 10),
-              customTextField(hint: 'search', controller: SearchController()),
-              SizedBox(height: 10),
-              customTextField(hint: 'All task', controller: notescontroler),
-              SizedBox(height: 10),
-              customTextField(
-                hint: 'New add task',
-                controller: taskcontroler,
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: () async {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Dashboardscreen()));
-                    try {
-                      await Auth().creatNotes(
-                        textcontroler.text,
-                        taskcontroler.text,
-                        notescontroler.text,
-                      );
-
-                      ScaffoldMessenger.of(
-                        context,
-                      ).showSnackBar(SnackBar(content: Text("add task")));
-                    } catch (e) {
-                      ScaffoldMessenger.of(
-                        context,
-                      ).showSnackBar(SnackBar(content: Text("$e")));
-                    }
-                  },
-                  child: Text(
-                    "ADD Folders",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+           customTextField(hint: 'your name' , controller: namecontroller),
+           SizedBox(height: 30,),
+           customTextField(hint: 'your work'),
+           SizedBox(height: 30,),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(onPressed: () async{
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>text_page()));
+                try{
+                  await authrepo().textclass(namecontroller.text, workcontroller.text);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: CustomText(text: 'task added',
+                   Color: AppColors.black,
+                    fontSize: 18),));
+                }catch (e){
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$e')));
+                }
+              }, child:CustomText(text: 'Next page', Color: AppColors.black, fontSize: 16) ),
+            )
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:notesapp/auth_repo.dart';
+// import 'package:notesapp/components%20folder/custom_field.dart';
+// import 'package:notesapp/components%20folder/custom_text.dart';
+// import 'package:notesapp/dashbord_page.dart';
+// import 'package:notesapp/fire_store/repo_class.dart';
+// import 'package:notesapp/utils/app_colors.dart';
+
+// class Folderscreen extends StatefulWidget {
+//   Folderscreen({super.key});
+
+//   @override
+//   State<Folderscreen> createState() => _FolderscreenState();
+// }
+
+// class _FolderscreenState extends State<Folderscreen> {
+//   TextEditingController textcontroler = TextEditingController();
+
+//   TextEditingController notescontroler = TextEditingController();
+
+//   TextEditingController taskcontroler = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         body: Padding(
+//           padding: EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               CustomText(
+//                   text: 'Folders adding', Color: AppColors.black, fontSize: 18),
+//               SizedBox(height: 10),
+//               customTextField(hint: 'search', controller: SearchController()),
+//               SizedBox(height: 10),
+//               customTextField(hint: 'All task', controller: notescontroler),
+//               SizedBox(height: 10),
+//               customTextField(
+//                 hint: 'New add task',
+//                 controller: taskcontroler,
+//               ),
+//               SizedBox(height: 10),
+//               SizedBox(
+//                 width: double.infinity,
+//                 child: ElevatedButton(
+//                   // style: ElevatedButton.styleFrom(
+//                   //   backgroundColor: Colors.blue,
+//                   //   padding: EdgeInsets.symmetric(vertical: 15),
+//                   //   shape: RoundedRectangleBorder(
+//                   //     borderRadius: BorderRadius.circular(12),
+//                   //   ),
+//                   // ),
+//                   onPressed: () async {
+//                     Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (context) => Dashboardscreen()));
+//                     try {
+//                       await Auth().creatNotes(
+//                         textcontroler.text,
+//                         taskcontroler.text,
+//                         notescontroler.text,
+//                       );
+
+//                       ScaffoldMessenger.of(
+//                         context,
+//                       ).showSnackBar(SnackBar(content: Text("add task")));
+//                     } catch (e) {
+//                       ScaffoldMessenger.of(
+//                         context,
+//                       ).showSnackBar(SnackBar(content: Text("$e")));
+//                     }
+//                   },
+//                   child: Text(
+//                     "ADD Folders",
+//                     style: TextStyle(fontSize: 18, color: Colors.white),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
